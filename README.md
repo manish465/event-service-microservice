cc1408b64f432861ef32aff905370c83afc7c3b0
manish <ms5978307@gmail.com>
2023-08-23 12:02:32 +0530

    added project readme file

---
+++ README.md
@@ -0,0 +1,8 @@
+# Event Microservice
+### This project is made using java, spring boot and spring cloud.
+
+
+below is the UML diagram for this project:
+![event-service-ms](./design/event-service-ms.drawio.png)
+
+[Here is the requirement file for the project](./design/event-service-microservice-req.md)
--- user-service/src/main/java/com/manish/user/controller/UserController.java
+++ user-service/src/main/java/com/manish/user/controller/UserController.java
@@ -8,7 +8,6 @@ import com.manish.user.entity.User;
 import com.manish.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
